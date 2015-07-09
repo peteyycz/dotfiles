1078c5bb542da576d1a1a01f3e1bdca88689ca6f
Peter Czibik <p.czibik@gmail.com>
2015-07-09 08:11:22 +0200

    Full rework on dotfiles, removed unnesessary files and simplified repo

--- install.sh
+++ install.sh
@@ -1,9 +1,19 @@
+#!/bin/bash
+
 echo "installing dotfiles"
 
 echo "initializing submodule(s)"
 git submodule update --init --recursive
 
-source install/link.sh
+DOTFILES=$HOME/.dotfiles
+
+echo "creating symlinks"
+linkables=$( ls -1 -d *.symlink )
+for file in $linkables ; do
+    target="$HOME/.$( basename $file ".symlink" )"
+    echo "creating symlink for $file"
+    ln -s $DOTFILES/$file $target
+done
 
 echo "configuring zsh as default shell"
 chsh -s $(which zsh)
