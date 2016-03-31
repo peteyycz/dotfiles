da063a6ec8ca901771771b27d843d935769d255c
Peter Czibik <p.czibik@gmail.com>
2016-04-01 01:00:16 +0200

    changed install script to include vim plug and linking

--- install.sh
+++ install.sh
@@ -18,9 +18,13 @@ done
 
 # Install node version manager
 echo "installing nvm"
-if [ ! -e ~/.nvm ]; then
-  wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash
-fi
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+
+echo "installing plug.vim"
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo "creating nvim symlink"
+ln -s $DOTFILES/config/nvim/* ~/.config/nvim/
 
 # Change default shell to ZSH
 echo "changing default shell to zsh"
