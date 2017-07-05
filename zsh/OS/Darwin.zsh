3a0f4ad2254f1a795465ed697c75f90545834996
Peter Czibik <p.czibik@gmail.com>
2017-07-05 10:45:10 +0200

    update config

--- zsh/OS/Darwin.zsh
+++ zsh/OS/Darwin.zsh
@@ -3,4 +3,8 @@
 # ====================
 test -e ${HOME}/.iterm2_shell_integration.bash && source ${HOME}/.iterm2_shell_integration.bash
 
-source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc'
+
+if [ -f '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc' ]; then
+    source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc'
+fi
+
