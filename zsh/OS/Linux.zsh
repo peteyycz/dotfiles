c01ece6aec17c38eb5ec5c1b4f62bba791ed01eb
Peter Czibik <p.czibik@gmail.com>
2018-01-09 23:08:47 +0100

    remove useless zsh plugins

--- zsh/OS/Linux.zsh
+++
@@ -1,7 +0,0 @@
-# BSD like copy
-alias pbcopy='xsel --clipboard --input'
-alias pbpaste='xsel --clipboard --output'
-
-# Linuxbrew config
-export PATH="$HOME/.linuxbrew/bin:$PATH"
-export MANPATH="$HOME/.linuxbrew/bin:$PATH"
