bbc1c1636b8095eb80e398057f508811c217f88e
Peter Czibik <dev@peterczibik.com>
2024-09-15 22:17:40 +0200

    Add prepare script

---
+++ prepare.sh
@@ -0,0 +1,8 @@
+# starship
+curl -sS https://starship.rs/install.sh | sh
+
+# asdf-vm
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.1
+
+# antigen
+curl -L git.io/antigen > $HOME/antigen.zsh
