ae0f33bba0b1c53caa61980aedea77cf5f41662f
Peter Czibik <dev@peterczibik.com>
2025-06-04 15:08:53 +0200

    Go stufF

--- common/.config/fish/config.fish
+++ common/.config/fish/config.fish
@@ -7,11 +7,11 @@ if status is-interactive
     # Variables {
     set -x EDITOR vim
     set -x VISUAL vim
-    set -x GOPATH "$HOME/Code"
     set -x GHQ_ROOT "$GOPATH/src"
-    # }
 
-    # Alias {
+    set -x GOPATH "$HOME/Code"
+    fish_add_path $GOPATH/bin
+
     alias gc="git commit"
     alias gco="git checkout"
     alias gp="git push"
@@ -21,6 +21,8 @@ if status is-interactive
     alias gd="git diff"
     alias gds="git diff --staged"
     alias gaa="git add --all"
+    alias grbc="git rebase --continue"
+    alias grba="git rebase --abort"
 
     alias tmuxn='tmux new-session -t $(basename $PWD)';
     alias vim='nvim';
@@ -30,5 +32,8 @@ if status is-interactive
 
     fish_add_path (go env GOBIN)
 
-    fish_add_path /opt/homebrew/bin
+    switch (uname)
+      case Darwin
+        fish_add_path /opt/homebrew/bin
+    end
 end
