3e1c1b75c4f2136adbcb613e66dc639b969633f1
Peter Czibik <dev@peterczibik.com>
2025-12-12 14:33:11 +0100

    Back to gnome

--- linux/.config/home-manager/home.nix
+++ linux/.config/home-manager/home.nix
@@ -25,12 +25,6 @@
     (writeShellScriptBin "zellij-session-picker" (builtins.readFile ./scripts/zellij-session-picker))
   ];
 
-  dconf.settings = {
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";
-    };
-  };
-
   fonts.fontconfig.enable = true;
 
   programs.home-manager.enable = true;
@@ -131,6 +125,125 @@
     settings = {
       legacy_version_file = true;
       idiomatic_version_file_enable_tools = [ "node" ];
+    };
+  };
+
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      enable-animations = true;
+    };
+    "org/gnome/mutter" = {
+      overlay-key = "";
+      center-new-windows = true;
+    };
+    "org/gnome/shell/keybindings" = {
+      switch-to-application-1 = [];
+      switch-to-application-2 = [];
+      switch-to-application-3 = [];
+      switch-to-application-4 = [];
+      switch-to-application-5 = [];
+      switch-to-application-6 = [];
+      switch-to-application-7 = [];
+      switch-to-application-8 = [];
+      switch-to-application-9 = [];
+      toggle-quick-settings = [];
+      toggle-message-tray = [];
+    };
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+      ];
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      name = "Ulauncher";
+      command = "ulauncher-toggle";
+      binding = "<Super>space";
+    };
+    "org/gnome/desktop/wm/keybindings" = {
+      activate-window-menu = [];
+      always-on-top = [];
+      begin-move = [];
+      begin-resize = [];
+      close = [ "<Super>q" ];
+      cycle-group = [];
+      cycle-group-backward = [];
+      cycle-panels = [];
+      cycle-panels-backward = [];
+      cycle-windows = [];
+      cycle-windows-backward = [];
+      lower = [];
+      maximize = [];
+      maximize-horizontally = [];
+      maximize-vertically = [];
+      minimize = [];
+      move-to-center = [];
+      move-to-corner-ne = [];
+      move-to-corner-nw = [];
+      move-to-corner-se = [];
+      move-to-corner-sw = [];
+      move-to-monitor-down = [];
+      move-to-monitor-left = [];
+      move-to-monitor-right = [];
+      move-to-monitor-up = [];
+      move-to-side-e = [];
+      move-to-side-n = [];
+      move-to-side-s = [];
+      move-to-side-w = [];
+      move-to-workspace-1 = [ "<Shift><Super>1" ];
+      move-to-workspace-2 = [ "<Shift><Super>2" ];
+      move-to-workspace-3 = [ "<Shift><Super>3" ];
+      move-to-workspace-4 = [ "<Shift><Super>4" ];
+      move-to-workspace-5 = [ "<Shift><Super>5" ];
+      move-to-workspace-6 = [ "<Shift><Super>6" ];
+      move-to-workspace-7 = [ "<Shift><Super>7" ];
+      move-to-workspace-8 = [ "<Shift><Super>8" ];
+      move-to-workspace-9 = [ "<Shift><Super>9" ];
+      move-to-workspace-10 = [];
+      move-to-workspace-11 = [];
+      move-to-workspace-12 = [];
+      move-to-workspace-down = [ "<Control><Shift><Alt>Down" ];
+      move-to-workspace-up = [ "<Control><Shift><Alt>Up" ];
+      move-to-workspace-last = [];
+      move-to-workspace-left = [];
+      move-to-workspace-right = [];
+      panel-main-menu = [ "<Alt>F1" ];
+      panel-run-dialog = [];
+      raise = [];
+      raise-or-lower = [];
+      set-spew-mark = [];
+      show-desktop = [];
+      switch-applications = [ "<Alt>Tab" ];
+      switch-applications-backward = [ "<Shift><Alt>Tab" ];
+      switch-group = [ "<Super>Above_Tab" "<Alt>Above_Tab" ];
+      switch-group-backward = [ "<Shift><Super>Above_Tab" "<Shift><Alt>Above_Tab" ];
+      switch-input-source = [];
+      switch-input-source-backward = [];
+      switch-panels = [];
+      switch-panels-backward = [];
+      switch-to-workspace-1 = [ "<Super>1" ];
+      switch-to-workspace-2 = [ "<Super>2" ];
+      switch-to-workspace-3 = [ "<Super>3" ];
+      switch-to-workspace-4 = [ "<Super>4" ];
+      switch-to-workspace-5 = [ "<Super>5" ];
+      switch-to-workspace-6 = [ "<Super>6" ];
+      switch-to-workspace-7 = [ "<Super>7" ];
+      switch-to-workspace-8 = [ "<Super>8" ];
+      switch-to-workspace-9 = [ "<Super>9" ];
+      switch-to-workspace-10 = [];
+      switch-to-workspace-11 = [];
+      switch-to-workspace-12 = [];
+      switch-to-workspace-down = [ "<Control><Alt>Down" ];
+      switch-to-workspace-up = [ "<Control><Alt>Up" ];
+      switch-to-workspace-last = [];
+      switch-to-workspace-left = [];
+      switch-to-workspace-right = [];
+      switch-windows = [];
+      switch-windows-backward = [];
+      toggle-above = [];
+      toggle-fullscreen = [];
+      toggle-maximized = [];
+      toggle-on-all-workspaces = [];
+      unmaximize = [];
     };
   };
 }
--- linux/.config/waybar/scripts/vpn-status.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-vpn=$(nmcli -t -f NAME,TYPE connection show --active | grep vpn | cut -d: -f1)
-if [ -n "$vpn" ]; then
-    echo "{\"text\": \"$vpn\", \"class\": \"connected\", \"tooltip\": \"Connected to $vpn\"}"
-else
-    echo "{\"text\": \"VPN\", \"class\": \"disconnected\", \"tooltip\": \"Not connected\"}"
-fi
