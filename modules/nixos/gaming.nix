711d5cd32ea874f913a349f45d7d2c22457a0f39
Peter Czibik <dev@peterczibik.com>
2026-05-06 20:14:17 +0200

    additional gaming stuff

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -1,9 +1,17 @@
 { ... }:
 {
   flake.modules.nixos.gaming =
-    { ... }:
+    { pkgs, ... }:
     {
       programs.steam.enable = true;
+      programs.steam.gamescopeSession.enable = true;
+
       networking.networkmanager.wifi.powersave = false;
+
+      environment.systemPackages = with pkgs; [
+        mangohud
+      ];
+
+      programs.gamemode.enable = true;
     };
 }
