4ae5f7c53dd8f67b9c1a2987150eaf5b5ce9ebe2
Peter Czibik <dev@peterczibik.com>
2026-05-07 16:35:04 +0200

    remove mouse

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -380,7 +380,6 @@
           bindm = [
             "$mod, mouse:272, movewindow"
             "$mod, mouse:273, resizewindow"
-            ", mouse:274, movewindow"
           ];
         };
       };
